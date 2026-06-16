10243f5dec2b25b89f8292db208934dd77db86e1
RaulRamos182 <raulramosmarin517@gmail.com>
2026-06-16 10:30:21 +0200

    Add message for repository change in main method

--- GitModeloA.java
+++ GitModeloA.java
@@ -4,7 +4,7 @@ public class GitModeloA {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Cambio en principal");
-
+		System.out.println("Cambio en el repositorio");
 		System.out.println("Cambio en Rama1");
 
 
