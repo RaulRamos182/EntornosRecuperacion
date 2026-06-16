ddc01bcf38e5fdc8f27250ac22d8fba965892cb4
Raul <raulramos@gmail.com>
2026-06-16 10:22:31 +0200

    Cambio en Main

--- GitModeloA.java
+++ GitModeloA.java
@@ -3,7 +3,7 @@ public class GitModeloA {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Hola a Tod@s");
+		System.out.println("Cambio en principal");
 
 
 	}
